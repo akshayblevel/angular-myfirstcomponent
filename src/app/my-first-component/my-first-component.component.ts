19b97d3fd198be62476c412cc7d66fe6fe63080d
akshayblevel <akshayblevel@gmail.com>
2021-08-19 18:49:17 +0530

    3 files modified, 3 added, 1 deleted

---
+++ src/app/my-first-component/my-first-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-component',
+  templateUrl: './my-first-component.component.html',
+  styleUrls: ['./my-first-component.component.css']
+})
+export class MyFirstComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
